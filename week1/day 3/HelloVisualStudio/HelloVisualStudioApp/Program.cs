2b2dfc70528a74381fb17f966e93afe4c9e255d1
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-11-28 11:57:38 -0600

    Make Hello World Visual Studio

---
+++ week1/day 3/HelloVisualStudio/HelloVisualStudioApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloVisualStudioApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //F5, Ctrl+F5 to run code
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
