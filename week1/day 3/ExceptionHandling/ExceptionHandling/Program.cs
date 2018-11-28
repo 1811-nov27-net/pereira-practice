fe6820cd6c893b9a4d5acd9c62170729c531c525
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-11-28 17:03:22 -0600

    Exception handling

--- week1/day 3/ExceptionHandling/ExceptionHandling/Program.cs
+++ week1/day 3/ExceptionHandling/ExceptionHandling/Program.cs
@@ -6,7 +6,54 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * Exceptions are runtime errors that we can potencially handle
+             * exceptions are obects and defined by classes like anything else
+             */
+
+            try
+            {
+                BadCode();
+
+
+                //trye goes around code that is expected to thorw an exception
+                var x = 4;
+                var y = x / 0;
+
+                Console.WriteLine("Never prints because excpetion is thrown first");
+
+                throw new ArgumentNullException();
+            }
+            catch(DivideByZeroException e)
+            {
+                //handle the exception in catch block
+                Console.WriteLine("divided by zero, moving on");
+                //at the end of catch, we move on with business
+            }
+            catch(InvalidCastException e)
+            {
+                Console.WriteLine("handled bad cast");
+
+                throw; //Re-throws the current exception
+                //only works inside catch
+            }
+
+            Console.WriteLine("the program continues");
+        }
+
+        static void BadCode()
+        {
+            try
+            {
+                object o = true;
+                string s = (string)o;
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("won't print beacause this isn't a fiv by 0");
+            }
+
+            Console.WriteLine("won't print");
         }
     }
 }
