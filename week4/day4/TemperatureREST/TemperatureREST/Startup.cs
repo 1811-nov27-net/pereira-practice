c2f194f26874dc985f22a00bcb97b10c4b219c65
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-21 11:49:01 -0600

    Temperature

--- week4/day4/TemperatureREST/TemperatureREST/Startup.cs
+++ week4/day4/TemperatureREST/TemperatureREST/Startup.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,12 +30,56 @@ namespace TemperatureREST
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<IdentityDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("TempAuthDB")));
 
-            // Register the Swagger generator, defining 1 or more Swagger documents
+            services
+                .AddIdentity<IdentityUser, IdentityRole>(options =>
+                {
+                    // Password settings (defaults - optional)
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireNonAlphanumeric = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequiredUniqueChars = 1;
+
+                    options.User.AllowedUserNameCharacters =
+                            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                })
+                .AddEntityFrameworkStores<IdentityDbContext>();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "TempApiAuth";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.Events = new CookieAuthenticationEvents
+                {
+                    OnRedirectToLogin = ctx =>
+                    {
+                        ctx.Response.StatusCode = 401; // Unauthorized (really, unauthenticated)
+                        return Task.FromResult(0);
+                    },
+                    OnRedirectToAccessDenied = ctx =>
+                    {
+                        ctx.Response.StatusCode = 403; // Forbidden (unauthorized, you're logged in, but you 
+                                                        // don't have the right role
+                        return Task.FromResult(0);
+                    },
+                };
+            });
+
+            services.AddAuthentication();
+
+            services
+                .AddMvc()
+                .AddXmlSerializerFormatters() // adds support for XML serialize & deserialize
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // add Swashbuckle Swagger UI middleware
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "TempoRecord", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "TempRecord API", Version = "v1" });
             });
         }
 
@@ -48,6 +96,8 @@ namespace TemperatureREST
                 app.UseHsts();
             }
 
+            app.UseAuthentication();
+
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
@@ -55,7 +105,7 @@ namespace TemperatureREST
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TempoRecord V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TempRecord API v1");
             });
 
             app.UseHttpsRedirection();
